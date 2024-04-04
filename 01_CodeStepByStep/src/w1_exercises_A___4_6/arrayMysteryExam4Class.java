8f164452aa258f5cf1f70d6974ca8d7ca33b1f64
Nathan Willett <natewillett1996@gmail.com>
2024-04-04 12:06:06 -0700

    project folder complete?

--- 01_CodeStepByStep/src/w1_exercises_A___4_6/arrayMysteryExam4Class.java
+++ 01_CodeStepByStep/src/w1_exercises_A___4_6/arrayMysteryExam4Class.java
@@ -3,6 +3,10 @@ package CodeStepByStep.src.w1_exercises_A___4_6;
 import java.util.Arrays;
 
 public class arrayMysteryExam4Class {
+    public static void main(String[] args) {
+        
+    }
+    
     public static void arrayMysteryExam4(int[] a) {
         for (int i = 1; i < a.length; i++) {
             a[i] = a[a.length - 1 - i] - a[i - 1];
