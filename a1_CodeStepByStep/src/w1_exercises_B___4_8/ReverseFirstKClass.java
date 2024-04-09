7ed9d28722a8968eb40963fd0273e28917c5d36c
Nathan Willett <natewillett1996@gmail.com>
2024-04-09 12:24:52 -0700

    lab 1 part b

--- a1_CodeStepByStep/src/w1_exercises_B___4_8/ReverseFirstKClass.java
+++ a1_CodeStepByStep/src/w1_exercises_B___4_8/ReverseFirstKClass.java
@@ -4,10 +4,24 @@ reverseFirstK(a, 5) should reverse the first 5 elements, modifying it to store {
 of k is 1, 0, negative, or greater than the length of the array, do not modify the array. */
 public class ReverseFirstKClass {
    public static void main(String[] args) {
-      int[] a = {10, -2, 33, 55, 9, 17, 6};
+      int[] a = { 10, -2, 33, 55, 9, 17, 6 };
       reverseFirstK(a, 4);
+
+      for (int value : a) {
+         System.out.print(value + " ");
+      }
    }
-   
+
    public int[] reverseFirstK(int[] list, int k) {
-      for (int i = 
+      if (k > 0 && k <= list.length) {
+         int temp;
+         if (k > 0 && k <= list.length) {
+            for (int i = 0; i < k / 2; i++) {
+               temp = list[i];
+               list[i] = list[k - i - 1];
+               list[k - i - 1] = temp;
+            }
+         }
+      }
    }
+}
