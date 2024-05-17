3e1fad027c0577c253206056d07cc245b702c7ad
Nathan Willett <natewillett1996@gmail.com>
2024-05-17 12:12:11 -0700

    upload test files

---
+++ a4_Projects/project5__5_16/app/src/test/java/project5__5_16/TestExpression.java
@@ -0,0 +1,24 @@
+package a4_Projects.project5__5_16.app.src.test.java.project5__5_16;
+
+// Adapted by Allison Obourn 
+// from a test program by Stuart Reges
+
+// Grammar Solver
+// CS 142
+//
+// Short program to test whether the expressions generated using the file
+// sentence2.txt are legal.  They can often by very long.  You should include
+// the expression as indicated below and you should be able to compile the
+// program.  It might throw an exception when run, but it should compile.
+
+import static java.lang.Math.*;   // so we can write abs instead of Math.abs
+
+@SuppressWarnings("unused")
+public class TestExpression {
+    public static void main(String[] args) {
+        double x = 3;
+        double y = 4;
+        // double z = <fill in expression here>;
+        // System.out.println(z);
+    }
+}
